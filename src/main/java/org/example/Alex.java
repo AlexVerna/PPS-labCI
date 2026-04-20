8a9441760dd3096e05fc3bc581eba356bb30cbaf
Alessandro <alessandro.verna@studio.unibo.it>
2026-04-20 11:58:28 +0200

    Add step 7

--- src/main/java/org/example/Alex.java
+++
@@ -1,8 +0,0 @@
-package org.example;
-
-public class Alex {
-
-    static void main() {
-        System.out.println("Hello World!");
-    }
-}
---
+++ src/main/scala/org.example/AlexScala.scala
@@ -0,0 +1,6 @@
+import org.example.AlexJava
+
+@main def hello(): Unit = {
+  println("Hello, World form Scala!")
+  AlexJava.printHelloWorldFromJava()
+}
